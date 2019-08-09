29757cf07fae4ce16b53caea5e1b0618d828dea7
nbkhope <ryumemaru@yahoo.com>
2019-08-08 21:02:21 -0700

    Add npm run build command and set things up so it builds to the build/ directory

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     main: './src/index.js'
   },
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, 'build'),
     publicPath: '/',
     filename: 'bundle.js'
   },
@@ -25,6 +25,6 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: './public'
   }
 };
